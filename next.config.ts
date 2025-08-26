015b83d7d949f080088be3eb38495faa846b4096
HkaanUsta <egemen.kaan@hotmail.com>
2025-08-26 23:11:05 +0300

    github actions update 6

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  basePath: '/metime-spa',
-
-  output: 'export',
-
-  images: {
-    unoptimized: true,
-  },
-
 };
 
 export default nextConfig;
